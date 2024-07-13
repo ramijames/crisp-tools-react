1f97d5e5a3ab17b4484918e44459285fb1685a85
Rami James <ramijames@gmail.com>
2024-07-13 14:40:57 -0400

    Color picker up and running

--- src/pages/color-tools/color-palette-generator.js
+++ src/pages/color-tools/color-palette-generator.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
 import ToolHeader from '../../components/common/ToolHeader';
 import BackTo from '../../components/common/BackTo';
+import style from './color-palette-generator.module.css';
+import { HexColorPicker } from 'react-colorful';
 
 function ColorPaletteGeneratorPage () {
+  const [color, setColor] = useState("#aabbcc");
+
+  function ConvertHexToRGB(hex) {
+    const r = parseInt(hex.substring(1, 3), 16);
+    const g = parseInt(hex.substring(3, 5), 16);
+    const b = parseInt(hex.substring(5, 7), 16);
+    return [ r, g, b ];
+  }
+
+  const [red, green, blue] = ConvertHexToRGB(color);
+
   return (
     <main className="CrispTool">
       <BackTo to="/color-tools">
@@ -13,6 +26,19 @@ function ColorPaletteGeneratorPage () {
       </BackTo>
       <section className="ToolPanel">
         <ToolHeader title="Color Palette Generator" description="Generate a color palette based on a single color" />
+        <section className={style.ColorPaletteGenerator}>
+          <section className={style.ColorPaletteSelect}>
+            <HexColorPicker color={color} onChange={setColor} />
+            <div className={style.ColorOutputRGB}>
+              <div>R <span className={style.number}>{red}</span></div>
+              <div>G <span className={style.number}>{green}</span></div>
+              <div>B <span className={style.number}>{blue}</span></div>
+            </div>
+          </section>
+          <section className={style.ColorPaletteOutput}>
+            temp
+          </section>
+        </section>
       </section>
     </main>
   );
