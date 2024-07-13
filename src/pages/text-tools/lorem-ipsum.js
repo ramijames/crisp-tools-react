63698872e291fd510543c0e159c1fe6f908f1436
Rami James <ramijames@gmail.com>
2024-07-13 11:02:24 -0400

    Lorem ipsum generator is working for setting paragraphs

--- src/pages/text-tools/lorem-ipsum.js
+++ src/pages/text-tools/lorem-ipsum.js
@@ -1,6 +1,33 @@
+import React, { useEffect, useState } from 'react';
 import BackTo from '../../components/atoms/BackTo';
+import styles from './lorem-ipsum.module.css';
 
-function LoremIpsum () {
+const { LoremIpsum } = require('lorem-ipsum');
+
+function LoremIpsumPage () {
+
+  // State for input values
+  const [numParagraphs, setNumParagraphs] = useState(1);
+  const [generatedText, setGeneratedText] = useState('');
+
+  useEffect(() => {
+    const lorem = new LoremIpsum({
+      sentencesPerParagraph: {
+        max: 8,
+        min: 3
+      },
+      wordsPerSentence: {
+        max: 32,
+        min: 8
+      }
+    });
+
+    const text = lorem.generateParagraphs(parseInt(numParagraphs, 10));
+
+    setGeneratedText(text);
+  
+  }, [numParagraphs]);
+
   return (
     <main className="CrispTool">
       <BackTo to="/text-tools">
@@ -11,9 +38,19 @@ function LoremIpsum () {
       </BackTo>
       <section className="ToolPanel">
         <h1>Lorem Ipsum Generator</h1>
+        <p>Generate placeholder text for your projects.</p>
+        <div className="ToolPanel__form">
+          <div className="ToolPanel__form__input">
+            <label htmlFor="numParagraphs">Number of Paragraphs</label>
+            <input type="number" id="numParagraphs" value={numParagraphs} onChange={(e) => setNumParagraphs(e.target.value)} />
+          </div>
+        </div>
+        <div className={styles.ToolPanel__output}>
+          <p>{generatedText}</p>
+        </div>
       </section>
     </main>
   )
 }
 
-export default LoremIpsum;
+export default LoremIpsumPage;
