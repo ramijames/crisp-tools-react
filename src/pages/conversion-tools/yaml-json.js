3a3a8c62645132debb4d63f093a8a5262e7caab5
Rami James <ramijames@gmail.com>
2024-07-15 16:46:04 -0400

    YAML<>JSON conversion now works both ways with a nice switch

--- src/pages/conversion-tools/yaml-json.js
+++ src/pages/conversion-tools/yaml-json.js
@@ -6,20 +6,53 @@ import style from './conversiontools.module.css';
 import jsYaml from 'js-yaml';
 
 function JsonYamlConverter() {
+  const [mode, setMode] = useState('YAMLtoJSON');
   const [yamlInput, setYamlInput] = useState('');
-  const [jsonOutput, setJsonOutput] = useState('');
+  const [jsonInput, setJsonInput] = useState('');
+  const [conversionOutput, setConversionOutput] = useState('');
 
   const handleYamlInputChange = (event) => {
-    setYamlInput(event.target.value);
+    if (mode === 'YAMLtoJSON') {
+      setYamlInput(event.target.value);
+    } else {
+      setJsonInput(event.target.value);
+    }
+  };
+
+  const toggleMode = () => {
+    setMode(mode === 'YAMLtoJSON' ? 'JSONtoYAML' : 'YAMLtoJSON');
+    
+    // Clear to default empty state
+    setYamlInput('');
+    setJsonInput('');
+    setConversionOutput('');
+  };
+
+  const convert = () => {
+    if (mode === 'YAMLtoJSON') {
+      convertYamlToJson();
+    } else {
+      convertJsonToYaml();
+    }
   };
 
   const convertYamlToJson = () => {
     try {
-      const result = jsYaml.load(yamlInput);
-      setJsonOutput(JSON.stringify(result, null, 2));
+      const parsedInput = jsYaml.load(yamlInput);
+      const conversionOutput = JSON.stringify(parsedInput, null, 2);
+      setConversionOutput(conversionOutput); 
     } catch (error) {
-      // Enhanced error message for clarity
-      setJsonOutput(`Error converting YAML to JSON: ${error.message}. Please check your YAML syntax, especially for correct indentation and colons after keys.`);
+      setConversionOutput(`Error converting YAML to JSON: ${error.message}. Please check your JSON syntax.`);
+    }
+  };
+  
+  const convertJsonToYaml = () => {
+    try {
+      const parsedInput = JSON.parse(jsonInput); 
+      const conversionOutput = jsYaml.dump(parsedInput);
+      setConversionOutput(conversionOutput); 
+    } catch (error) {
+      setConversionOutput(`Error converting JSON to YAML: ${error.message}. Please check your JSON syntax.`);
     }
   };
 
@@ -32,20 +65,35 @@ function JsonYamlConverter() {
         Back to Conversion Tools
       </BackTo>
       <section className="ToolPanel">
-        <ToolHeader title="YAML to JSON Converter" description="Quickly convert YAML to JSON format" />
+        <div className={style.ToolTop}>
+          {
+            mode === 'YAMLtoJSON' ? 
+            <ToolHeader title="YAML to JSON Converter" description="Quickly convert between formats" /> :
+            <ToolHeader title="JSON to YAML Converter" description="Quickly convert between formats" />
+          }
+          <div className={style.Toggle}>
+            {/* <Button onClick={toggleMode} className={style.Button}>Switch to {mode === 'YAMLtoJSON' ? 'YAML to JSON' : 'JSON to YAML'}</Button> */}
+            <label class={style.TypeToggle}>
+              <span>YAML to JSON</span>
+              <input type="checkbox" checked={mode === 'JSONtoYAML'} onChange={toggleMode} />
+              <div class={style.Slider}></div>
+              <span>JSON to YAML</span>
+            </label>
+          </div>
+        </div>
         <section className={style.Converter}>
           <section className={style.YamlConverterContent}>
             <textarea
-              value={yamlInput}
+              value={mode === 'YAMLtoJSON' ? yamlInput : jsonInput}
               onChange={handleYamlInputChange}
-              placeholder="Paste YAML here"
+              placeholder={mode === 'YAMLtoJSON' ? "Paste YAML here" : "Paste JSON here"}
               rows="10"
               className={style.Textarea}
             ></textarea>
             <section className={style.ButtonColumn}>
-              <Button onClick={convertYamlToJson} className={style.Button}>Convert to JSON</Button>
+              <Button onClick={convert} className={style.Button}>Convert to {mode === 'YAMLtoJSON' ? 'JSON' : 'YAML'}</Button>
             </section>
-            <pre className={style.Pre}>{jsonOutput}</pre>
+            <pre className={style.Pre}>{conversionOutput}</pre>
           </section>
           
         </section>
